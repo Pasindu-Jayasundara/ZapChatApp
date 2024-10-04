f2d115f21afcdc9d57e014b9f7e839d5125c0161
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2024-10-05 01:19:59 +0530

    code

--- ZapChat/app/singleChat.js
+++ ZapChat/app/singleChat.js
@@ -13,10 +13,78 @@ export default function singleChat() {
 
     const data = useLocalSearchParams();
 
+    useEffect(() => {
+
+        (async () => {
+ 
+            try {
+                if (getUser == "") {
+                    let sessionId = await AsyncStorage.getItem("user")
+                    if (sessionId == null) {
+
+                        await AsyncStorage.removeItem("verified");
+                        await AsyncStorage.removeItem("user");
+
+                        router.replace("/")
+
+                    } else {
+                        setUser(sessionId)
+                    }
+                }
+
+                let url = "https://redbird-suitable-conversely.ngrok-free.app/ZapChatBackend/SingleChat"
+
+                let obj = {
+                    searchText: getSearchText,
+                    category: getCategory
+                }
+                let response = await fetch(url, {
+                    method: "POST",
+                    body: JSON.stringify(obj),
+                    headers: {
+                        "Content-Type": "application/json",
+                        'Cookie': `JSESSIONID=${getUser}`
+                    }
+                })
+
+                if (response.ok) {
+
+                    let obj = await response.json()
+                    if (obj.success) {
+
+                        setChatDataArr(obj.data.data)
+                        setHeaderImage(obj.data.profile)
+
+                    } else {
+                        if (obj.data == "Please LogIn") {
+
+                            await AsyncStorage.removeItem("verified");
+                            await AsyncStorage.removeItem("user");
+
+                            router.replace("/")
+                        } else {
+                            Alert.alert(obj.data); 
+                        }
+                        console.log(obj.data)
+                    }
+
+                } else {
+                    Alert.alert("Please Try Again Later");
+                    console.log(response)
+                }
+
+            } catch (error) {
+                console.error(error)
+            }
+
+        })()
+
+    }, [])
+
     return (
         <SafeAreaView style={styles.safearea}>
 
-            <ChatHeader data={data}/>
+            <ChatHeader data={data} />
 
             <ScrollView contentContainerStyle={styles.body}>
 
---
+++ ZapChatBackend/src/java/model/SingleChatFilter.java
@@ -0,0 +1,30 @@
+package model;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter(urlPatterns = {"/SingleChat"})
+public class SingleChatFilter implements Filter{
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        
+
+    }
+
+    @Override
+    public void destroy() {
+    }
+    
+}
